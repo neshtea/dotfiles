e7a6c20d70f5f17a39a0a064b4be96c20c2cfcc0
Marco Schneider <marco.schneider@active-group.de>
2021-09-09 19:33:45 +0200

    Use vterm, add fish functions for vterm.

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -20,6 +20,7 @@ in
           epkgs.clj-refactor
           epkgs.company
           epkgs.consult
+          epkgs.crux
           epkgs.cycle-themes
           epkgs.deft
           epkgs.diff-hl
@@ -44,10 +45,12 @@ in
           epkgs.markdown-mode
           epkgs.modus-themes
           epkgs.nix-mode
-          epkgs.notmuch
+          epkgs.ol-notmuch
           epkgs.origami
           epkgs.orderless
           epkgs.org
+          epkgs.org-bullets
+          epkgs.org-present
           epkgs.org-contrib
           epkgs.org-roam
           epkgs.projectile
@@ -58,11 +61,49 @@ in
           epkgs.solaire-mode
           epkgs.use-package
           epkgs.utimeclock
+          epkgs.vterm
+          epkgs.vterm-toggle
           epkgs.which-key
           epkgs.yaml-mode
           epkgs.zoom
         ];
         package = pkgs.emacsMacport;
+      };
+
+      # For emacs/vterm
+      programs.fish.functions = {
+
+        # https://github.com/akermu/emacs-libvterm#message-passing
+        # TODO I can't be bothered to escape this function right now.
+        # vterm_cmd.body = ''
+        #   set -l vterm_elisp ()
+        #   for arg in $argv
+        #     set -a vterm_elisp (printf '"%s" ' (string replace -a -r '([\\\\"])' '\\\\\\\\$1' $arg))
+        #   end
+        #   vterm_printf '51;E'(string join '' $vterm_elisp)
+        # '';
+
+        # See https://github.com/akermu/emacs-libvterm#shell-side-configuration
+        vterm_printf.body = ''
+          if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+            # tell tmux to pass the escape sequences through
+            printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+          else if string match -q -- "screen*" "$TERM"
+            # GNU screen (screen, screen-256color, screen-256color-bce)
+            printf "\eP\e]%s\007\e\\" "$argv"
+          else
+            printf "\e]%s\e\\" "$argv"
+          end
+        '';
+
+        # https://github.com/akermu/emacs-libvterm#vterm-buffer-name-string
+        fish_title = ''
+          function fish_title
+            hostname
+            echo ":"
+            pwd
+          end
+        '';
       };
       xdg.configFile."emacs/init.el".source = ./init.el;
     };
