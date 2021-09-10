aff4a220e048b914a6aa6a03079c49b637623b9a
Marco Schneider <marco.schneider@active-group.de>
2021-09-10 09:57:21 +0200

    Cleanup fish config, add nix-env.fish.
    nix-env.fish is taken from https://github.com/lilyball/nix-env.fish.
    I recently realized that I had imperatively set the nix environment
    for fish via packer.  This fixes that and includes it in the
    home-manager setup.

--- modules/shell/fish/default.nix
+++ modules/shell/fish/default.nix
@@ -7,50 +7,64 @@ in
       enable = lib.mkEnableOption "fish";
     };
     config = lib.mkIf cfg.enable {
+      home.packages = [ pkgs.starship ];
       programs.fish = {
         enable = true;
+        functions = {
+          start_agent = {
+            body = ''
+              ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+              chmod 600 $SSH_ENV
+              . $SSH_ENV > /dev/null
+              ssh-add
+            '';
+            description = "Initialize new SSH agent.";
+          };
+          test_identities = {
+            body = ''
+              ssh-add -l | grep "The agent has no identities" > /dev/null
+              if [ $status -eq 0 ]
+                ssh-add
+                if [ $status -eq 2 ]
+                  start_agent
+                end
+              end
+            '';
+          };
+        };
+        promptInit = "${pkgs.starship}/bin/starship init fish | source";
         shellAbbrs = {
+          hme = "home-manager edit";
           hms = "home-manager switch";
         };
         shellInit = ''
-# See https://gist.github.com/gerbsen/5fd8aa0fde87ac7a2cae
-# content has to be in .config/fish/config.fish
-# if it does not exist, create the file
-setenv SSH_ENV $HOME/.ssh/environment
- function start_agent
-  echo "Initializing new SSH agent ..."
-  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-  echo "succeeded"
-  chmod 600 $SSH_ENV 
-  . $SSH_ENV > /dev/null
-  ssh-add
-end
- function test_identities
-        ssh-add -l | grep "The agent has no identities" > /dev/null
-  if [ $status -eq 0 ]
-    ssh-add
-    if [ $status -eq 2 ]
-        start_agent
-    end
-  end
-end
- if [ -n "$SSH_AGENT_PID" ] 
-  ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-  if [ $status -eq 0 ]
-    test_identities
-  end  
-else
-  if [ -f $SSH_ENV ]
-    . $SSH_ENV > /dev/null
-  end  
-  ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
-  if [ $status -eq 0 ]
-    test_identities
-  else 
-    start_agent
-  end  
-end
+          # See https://gist.github.com/gerbsen/5fd8aa0fde87ac7a2cae
+          # content has to be in .config/fish/config.fish
+          # if it does not exist, create the file
+          setenv SSH_ENV $HOME/.ssh/environment
+          if [ -n "$SSH_AGENT_PID" ]
+            ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+            if [ $status -eq 0 ]
+              test_identities
+            end
+          else
+            if [ -f $SSH_ENV ]
+              . $SSH_ENV > /dev/null
+            end
+            ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+            if [ $status -eq 0 ]
+              test_identities
+            else
+              start_agent
+            end
+          end
         '';
       };
+      # See https://github.com/lilyball/nix-env.fish
+      xdg.configFile."fish/conf.d/" = {
+        source = ./conf.d;
+        recursive = true;
+      };
+
     };
   }
