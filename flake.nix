2efa4d53894ec731f3d1a051b8d6ad2988ca91d0
Marco Schneider <marco.schneider@active-group.de>
2022-05-03 21:55:20 +0200

    Update flake.

--- flake.nix
+++ flake.nix
@@ -2,20 +2,26 @@
   description = "My nix home-manager configuration as a flake.";
 
   inputs = {
-    home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-darwin";
       username = "schneider";
+      pkgs = import nixpkgs {
+        config.allowUnfree = true;
+        inherit system;
+      };
     in {
       homeConfigurations.${username} =
         home-manager.lib.homeManagerConfiguration {
           configuration = import ./home.nix;
 
-          inherit system username;
+          inherit system username pkgs;
           homeDirectory = "/Users/${username}";
           stateVersion = "22.05";
         };
