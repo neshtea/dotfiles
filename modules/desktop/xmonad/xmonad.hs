fe9782dd4507ac2d5f16c67505f7e2f57ef3e611
Marco Schneider <marco.schneider@active-group.de>
2022-06-04 18:14:54 +0200

    Remove polybar from xmonad config, cleanup.

--- modules/desktop/xmonad/xmonad.hs
+++ modules/desktop/xmonad/xmonad.hs
@@ -12,32 +12,21 @@ import           XMonad.Actions.DynamicProjects        ( Project(..)
                                                        , dynamicProjects
                                                        , switchProjectPrompt
                                                        )
--- Imports for Polybar --
-import qualified Codec.Binary.UTF8.String              as UTF8
-import qualified DBus                                  as D
-import qualified DBus.Client                           as D
-import           XMonad.Hooks.DynamicLog
 
 main :: IO ()
-main = mkDbusClient >>= main'
-
-myTerminal = "kitty"
-appLauncher = "rofi -disable-history -show run"
-
-main' :: D.Client -> IO ()
-main' dbus = xmonad . docks . ewmhFullscreen . ewmh . dynProjects $ def
+main = xmonad . docks . ewmhFullscreen . ewmh . dynProjects $ def
     { modMask    = mod4Mask  -- Rebind Mod to the Super key
-    , terminal = myTerminal
+    , terminal = "kitty"
     , layoutHook = myLayout
     , borderWidth = 3
     , workspaces = myWs
     , normalBorderColor = "#1d2021"
     , focusedBorderColor = "#fbf1c7"
-    , logHook = myLogHook
     } `additionalKeysP`
     [ ("M-S-z" , spawn "xscreensaver-command -lock" )
     , ("M-f"   , spawn "firefox"                    )
-    , ("M-r"   , spawn appLauncher                  )
+    , ("M-r"   , spawn "rofi -disable-history -show run" )
+    , ("M-w"   , spawn "rofi -disable-history -show window" )
     ]
   where
     dynProjects = dynamicProjects projects
@@ -55,47 +44,7 @@ myLayout = avoidStruts $ mySpacing $ tiled ||| Mirror tiled ||| Full ||| threeCo
     ratio   = 1/2    -- Default proportion of screen occupied by master pane
     delta   = 3/100  -- Percent of screen to increment by when resizing panes
 
-mkDbusClient :: IO D.Client
-mkDbusClient = do
-    dbus <- D.connectSession
-    D.requestName dbus (D.busName_ "org.xmonad.log") opts
-    return dbus
-  where
-    opts = [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
-
-dbusOutput :: D.Client -> String -> IO ()
-dbusOutput dbus str =
-  let opath = D.objectPath_ "/org/xmonad/Log"
-      iname = D.interfaceName_ "org.xmonad.Log"
-      mname = D.memberName_ "Update"
-      signal = D.signal opath iname mname
-      body = [D.toVariant $ UTF8.decodeString str]
-  in D.emit dbus $ signal { D.signalBody = body }
-
-polybarHook :: D.Client -> PP
-polybarHook dbus =
-  let wrapper c s | s /= "NSP" = wrap ("%{F" <> c <> "} ") " %{F-}" s
-                  | otherwise  = mempty
-      blue   = "#2E9AFE"
-      gray   = "#7F7F7F"
-      orange = "#ea4300"
-      purple = "#9058c7"
-      red    = "#722222"
-  in  def { ppOutput          = dbusOutput dbus
-          , ppCurrent         = wrapper blue
-          , ppVisible         = wrapper gray
-          , ppUrgent          = wrapper orange
-          , ppHidden          = wrapper gray
-          , ppHiddenNoWindows = wrapper red
-          , ppTitle           = wrapper purple . shorten 90
-          }
-
-myPolybarLogHook dbus = myLogHook <+> dynamicLogWithPP (polybarHook dbus)
-
-myLogHook = fadeInactiveLogHook 0.9
-
 -- Workspaces
-
 homWs = "[1] home"
 devWs = "[2] dev"
 wrkWs = "[3] work"
