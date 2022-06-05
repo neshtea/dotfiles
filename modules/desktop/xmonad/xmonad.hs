e30c9ffd2176bc338c8f04651bbde8ea2d779827
Marco Schneider <marco.schneider@active-group.de>
2022-06-05 14:24:33 +0200

    Write xmonad-logger output to dbus for polybar to pick up.

--- modules/desktop/xmonad/xmonad.hs
+++ modules/desktop/xmonad/xmonad.hs
@@ -7,14 +7,22 @@ import XMonad.Layout.Magnifier
 import XMonad.Layout.Spacing
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
-import           XMonad.Hooks.FadeInactive             ( fadeInactiveLogHook )
-import           XMonad.Actions.DynamicProjects        ( Project(..)
-                                                       , dynamicProjects
-                                                       , switchProjectPrompt
-                                                       )
+import XMonad.Hooks.FadeInactive             ( fadeInactiveLogHook )
+import XMonad.Actions.DynamicProjects        ( Project(..)
+                                             , dynamicProjects
+                                             , switchProjectPrompt
+                                             )
+-- Imports for Polybar --
+import qualified Codec.Binary.UTF8.String              as UTF8
+import qualified DBus                                  as D
+import qualified DBus.Client                           as D
+import           XMonad.Hooks.DynamicLog
 
 main :: IO ()
-main = xmonad . docks . ewmhFullscreen . ewmh . dynProjects $ def
+main = mkDbusClient >>= main'
+
+main' :: D.Client -> IO ()
+main' dbus = xmonad . docks . ewmhFullscreen . ewmh . dynProjects $ def
     { modMask    = mod4Mask  -- Rebind Mod to the Super key
     , terminal = "kitty"
     , layoutHook = myLayout
@@ -22,9 +30,9 @@ main = xmonad . docks . ewmhFullscreen . ewmh . dynProjects $ def
     , workspaces = myWs
     , normalBorderColor = "#1d2021"
     , focusedBorderColor = "#fbf1c7"
+    , logHook = myPolybarLogHook dbus
     } `additionalKeysP`
-    [ ("M-S-z" , spawn "xscreensaver-command -lock" )
-    , ("M-f"   , spawn "firefox"                    )
+    [ ("M-f"   , spawn "firefox"                    )
     , ("M-r"   , spawn "rofi -disable-history -show run" )
     , ("M-w"   , spawn "rofi -disable-history -show window" )
     ]
@@ -45,19 +53,22 @@ myLayout = avoidStruts $ mySpacing $ tiled ||| Mirror tiled ||| Full ||| threeCo
     delta   = 3/100  -- Percent of screen to increment by when resizing panes
 
 -- Workspaces
-homWs = "[1] home"
-devWs = "[2] dev"
-wrkWs = "[3] work"
-chtWs = "[4] chat"
-mscWs = "[5] misc"
+homeWs, devWs, workWs, chatWs, musicWs, systemWs, miscWs :: WorkspaceId
+homeWs   = "[1] home"
+devWs    = "[2] dev"
+workWs   = "[3] work"
+chatWs   = "[4] chat"
+musicWs  = "[5] media"
+systemWs = "[6] system"
+miscWs   = "[7] misc"
 
 myWs :: [WorkspaceId]
-myWs = [ homWs, devWs, wrkWs, chtWs, mscWs ]
+myWs = [ homeWs, devWs, workWs, chatWs, musicWs, systemWs, miscWs ]
 
 -- Projects
 projects :: [Project]
 projects =
-  [ Project { projectName = homWs
+  [ Project { projectName = homeWs
             , projectDirectory = "~/"
             , projectStartHook = Nothing
             }
@@ -66,19 +77,69 @@ projects =
             , projectStartHook = Just $ do spawn "kitty"
                                            spawn "emacs"
             }
-  , Project { projectName = wrkWs
+  , Project { projectName = workWs
             , projectDirectory = "~/Repos"
             , projectStartHook = Just $ do spawn "thunderbird"
                                            spawn "mattermost-desktop"
             }
-  , Project { projectName = chtWs
+  , Project { projectName = chatWs
             , projectDirectory = "~/"
             , projectStartHook = Just $ do spawn "signal-desktop"
-                                           -- spawn "element-desktop"
-                                           -- spawn "telegram-desktop"
+                                           spawn "element-desktop"
+                                           spawn "telegram-desktop"
             }
-  , Project { projectName = mscWs
+  , Project { projectName = musicWs
             , projectDirectory = "~/"
-            , projectStartHook = Just $ spawn "kitty"
+            , projectStartHook = Just $ spawn "spotify"
+            }
+  , Project { projectName = systemWs
+            , projectDirectory = "~/"
+            , projectStartHook = Just $ do spawn "kitty htop"
+                                           spawn "pavucontrol"
+            }
+  , Project { projectName = miscWs
+            , projectDirectory = "~/"
+            , projectStartHook = Just $ spawn "keepassxc"
             }
   ]
+
+mkDbusClient :: IO D.Client
+mkDbusClient = do
+  dbus <- D.connectSession
+  D.requestName dbus (D.busName_ "org.xmonad.log") opts
+  return dbus
+ where
+  opts = [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+
+-- Emit a DBus signal on log updates
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str =
+  let opath  = D.objectPath_ "/org/xmonad/Log"
+      iname  = D.interfaceName_ "org.xmonad.Log"
+      mname  = D.memberName_ "Update"
+      signal = (D.signal opath iname mname)
+      body   = [D.toVariant $ UTF8.decodeString str]
+  in  D.emit dbus $ signal { D.signalBody = body }
+
+polybarHook :: D.Client -> PP
+polybarHook dbus =
+  let wrapper c s | s /= "NSP" = wrap ("%{F" <> c <> "} ") " %{F-}" s
+                  | otherwise  = mempty
+      normal = "#fbf1c7"
+      gray   = "#7F7F7F"
+      orange = "#ea4300"
+      purple = "#9058c7"
+      red    = "#722222"
+  in  def { ppOutput          = dbusOutput dbus
+          , ppCurrent         = wrapper normal
+          , ppVisible         = wrapper gray
+          , ppUrgent          = wrapper orange
+          , ppHidden          = wrapper gray
+          , ppHiddenNoWindows = wrapper red
+          -- i dont know how else to hide the window title
+          , ppTitle           = const "" -- shorten 100 . wrapper normal
+          }
+
+myPolybarLogHook dbus = myLogHook <+> dynamicLogWithPP (polybarHook dbus)
+
+myLogHook = fadeInactiveLogHook 0.9
