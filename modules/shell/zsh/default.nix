5646ca4276d10f6cd61593e1737527b5b2f27d31
Marco Schneider <marco.schneider@active-group.de>
2022-11-24 19:35:11 +0100

    Make tex play a little nicer.

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -15,8 +15,14 @@ in {
         enableAutosuggestions = true;
         enableSyntaxHighlighting = true;
         defaultKeymap = "emacs";
+        # NOTE I need the `TEXINPUTS` variable to build
+        # LaTeX-documents with my companies templates.  Since the repo
+        # this comes from is in mercurial, I can't set it as an input
+        # for my deriviation.  I'm responsible for checking out the
+        # repo by hand and put it in the correct location.
         initExtra = ''
           export PATH="$HOME/.bin:$PATH"
+          export TEXINPUTS="$HOME/repos/ag/howto/tex:$TEXINPUTS"
         '';
         initExtraBeforeCompInit = ''
           eval "$(starship init zsh)"
