e9ba4bcdc335e34b1ed06e1f48d406c51adf1d26
Marco Schneider <marco.schneider@active-group.de>
2023-03-08 21:24:50 +0100

    Remove xhyve.

--- modules/programs/docker/default.nix
+++ modules/programs/docker/default.nix
@@ -7,19 +7,6 @@ let cfg = config.modules.programs.docker;
 in {
   options.modules.programs.docker = { enable = lib.mkEnableOption "docker"; };
   config = lib.mkIf cfg.enable {
-    home = {
-      packages = with pkgs; [
-        docker
-        docker-compose
-        docker-machine
-        xhyve # for linux virtualization
-        docker-machine-xhyve # docker driver for xhyve
-      ];
-      activation = {
-        createDockerMachine = lib.hm.dag.entryAnywhere ''
-          docker-machine create default --driver xhyve --xhyve-experimental-nfs-share=true --xhyve-disk-size "40000"
-        '';
-      };
-    };
+    home = { packages = with pkgs; [ docker docker-compose ]; };
   };
 }
