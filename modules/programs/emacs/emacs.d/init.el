87f597c52ffbc10cd0309bf4e59bc572f53c180e
Marco Schneider <marco.schneider@active-group.de>
2023-08-04 10:37:53 +0200

    emacs: Remove neshtea/copy-line function.

--- modules/programs/emacs/emacs.d/init.el
+++ modules/programs/emacs/emacs.d/init.el
@@ -76,30 +76,6 @@ the face-font."
 (show-paren-mode 1)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; taken from https://www.emacswiki.org/emacs/CopyingWholeLines
-(defun neshtea/copy-line (arg)
-  "Copy lines (as many as prefix argument) in the kill ring.
-      Ease of use features:
-      - Move to start of next line.
-      - Appends the copy on sequential calls.
-      - Use newline as last char even on the last line of the buffer.
-      - If region is active, copy its lines."
-  (interactive "p")
-  (let ((beg (line-beginning-position))
-        (end (line-end-position arg)))
-    (when mark-active
-      (if (> (point) (mark))
-          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
-        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
-    (if (eq last-command 'copy-line)
-        (kill-append (buffer-substring beg end) (< end beg))
-      (kill-ring-save beg end)))
-  (kill-append "\n" nil)
-  (beginning-of-line (or (and arg (1+ arg)) 2))
-  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
-
-(global-set-key (kbd "C-c C-k") #'neshtea/copy-line)
-
 ;; Sometimes, I need relative line numbers.  `display-line-numbers`
 ;; has this built in.  This function makes it easier to toggle.
 
