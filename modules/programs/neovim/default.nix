c96caadfe6e1fbbdcb1af277cbe8d2da8a9b78ed
Marco Schneider <marco.schneider@active-group.de>
2023-10-18 10:03:48 +0200

    use a smaller, non nix-tracked nvim config

--- modules/programs/neovim/default.nix
+++ modules/programs/neovim/default.nix
@@ -15,14 +15,14 @@ in {
         jq # Formatter for json
         python310Packages.mdformat # Formatter for markdown
       ];
-      activation = {
-        symlinkNeovimConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          if [ ! -h $HOME/.config/nvim ]; then
-              $DRY_RUN_CMD mkdir -p $HOME/.config
-              $DRY_RUN_CMD ln -snf $HOME/dotfiles/modules/programs/neovim/nvim $HOME/.config/nvim
-          fi
-        '';
-      };
+      # activation = {
+      #   symlinkNeovimConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      #     if [ ! -h $HOME/.config/nvim ]; then
+      #         $DRY_RUN_CMD mkdir -p $HOME/.config
+      #         $DRY_RUN_CMD ln -snf $HOME/dotfiles/modules/programs/neovim/nvim $HOME/.config/nvim
+      #     fi
+      #   '';
+      # };
     };
   };
 }
