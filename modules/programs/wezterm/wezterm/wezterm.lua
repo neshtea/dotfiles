9c789185936db47927c3c7be65b848bd3b294777
Marco Schneider <marco.schneider@active-group.de>
2023-11-15 21:10:04 +0100

    wezterm: don't quit on close when on MacOS

--- modules/programs/wezterm/wezterm/wezterm.lua
+++ modules/programs/wezterm/wezterm/wezterm.lua
@@ -12,4 +12,11 @@ config.color_scheme = 'Gruvbox dark, pale (base16)'
 config.font = wezterm.font('Iosevka')
 config.font_size = 16.0
 
+local os = wezterm.target_triple
+
+if (os == 'aarch64-apple-darwin') or (os == 'x86_64-apple-darwin') then
+    -- behave like a real MacOS app
+    config.quit_when_all_windows_are_closed = false
+end
+
 return config
