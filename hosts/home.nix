b7427ef56b9bab17f4648e50b5347ad954ba5047
Marco Schneider <marco.schneider@active-group.de>
2024-01-10 12:07:37 +0100

    make ocaml tooling available system-wide

--- hosts/home.nix
+++ hosts/home.nix
@@ -33,6 +33,15 @@
     leiningen
     neovim-nightly
 
+    # Basic OCaml stuff if the project doesn't bring it.
+    dune_3
+    opam
+    ocamlPackages.merlin
+    ocamlPackages.ocamlformat
+    ocamlPackages.ocaml-lsp
+    ocamlPackages.utop
+    ocamlPackages.dune-release
+
     # most projects don't define a specific docker/docker-compose, so
     # let's have this available user-wide.
     docker
