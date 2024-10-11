39516417a9a3e5da32cf1e1a3e32fff87bd935a3
Marco Schneider <marco.schneider@active-group.de>
2024-10-11 09:59:07 +0200

    zsh: remove home-manager configuration, add config files

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -13,60 +13,18 @@ in
     enable = lib.mkEnableOption "zsh";
   };
   config = lib.mkIf cfg.enable {
-    programs = {
-      starship = {
-        enable = true;
-        enableZshIntegration = true;
-      };
-      zsh = {
-        enable = true;
-        enableCompletion = true;
-        autosuggestion.enable = true;
-        syntaxHighlighting.enable = true;
-        shellGlobalAliases = {
-          nrs = "nixos-rebuild switch --flake ~/dotfiles/ --use-remote-sudo";
-          serve = "${pkgs.python3}/bin/python3 -m http.server";
-          # git
-          gls = "git log --oneline";
-        };
-        defaultKeymap = "emacs";
-        # NOTE I need the `TEXINPUTS` variable to build
-        # LaTeX-documents with my companies templates.  Since the repo
-        # this comes from is in mercurial, I can't set it as an input
-        # for my deriviation.  I'm responsible for checking out the
-        # repo by hand and put it in the correct location.
-        initExtra = ''
-          export PATH="$HOME/bin:$PATH"
-          export TEXINPUTS="$HOME/repos/ag/howto/tex:$TEXINPUTS"
-          . "$HOME/.cargo/env"
-        '';
-        initExtraBeforeCompInit = ''
-          if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-            . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    home = {
+      activation = {
+        symlinkZsh = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          if [ ! -e $HOME/.zshrc ]; then
+            $DRY_RUN_CMD ln -snf $HOME/dotfiles/modules/shell/zsh/zshrc $HOME/.zshrc
           fi
-          eval "$(starship init zsh)"
+          if [ ! -c $HOME/.zshenv ]; then
+            $DRY_RUN_CMD ln -snf $HOME/dotfiles/modules/shell/zsh/zshenv $HOME/.zshenv
+          fi
         '';
-        oh-my-zsh = {
-          enable = true;
-          plugins = [
-            "git"
-            "sudo"
-            "brew"
-            "cabal"
-            "common-aliases"
-            "direnv"
-            "docker"
-            "docker-compose"
-            "fzf"
-            "gpg-agent"
-            "ripgrep"
-            "rust"
-            "ssh-agent"
-            "tmux"
-          ];
-          theme = "minimal";
-        };
       };
+      packages = [ pkgs.starship ];
     };
   };
 }
