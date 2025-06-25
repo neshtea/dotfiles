625309efa018833728c8bdea9be52c31575462c3
Marco Schneider <marco.schneider@active-group.de>
2025-06-25 10:40:33 +0200

    wezterm: Change theme based on system appearance.

--- modules/programs/wezterm/wezterm/wezterm.lua
+++ modules/programs/wezterm/wezterm/wezterm.lua
@@ -1,6 +1,22 @@
 local wezterm = require("wezterm")
 local config = {}
 
+-- https://wezterm.org/config/lua/wezterm.gui/get_appearance.html
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+local function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Macchiato'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
@@ -8,10 +24,10 @@ end
 -- tab bar
 config.enable_tab_bar = false
 
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = scheme_for_appearance(get_appearance()) -- 'Gruvbox Material (Gogh)'
 -- config.font = wezterm.font("JetBrains Mono")
-config.font = wezterm.font("Iosevka")
-config.font_size = 14.0
+config.font = wezterm.font("Menlo")
+config.font_size = 12.0
 config.adjust_window_size_when_changing_font_size = false
 config.audible_bell = "Disabled"
 
