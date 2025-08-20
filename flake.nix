bbc4d92b1816f5a0ee23d7b488fe7a3cd52f2aa6
Marco Schneider <marco.schneider@active-group.de>
2025-08-20 09:39:08 +0200

    Oxomoco: Remove from configuration for now.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
