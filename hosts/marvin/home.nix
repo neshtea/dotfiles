4e5d670d6dad22a64f1f27e6ec2928715ad182dd
Marco Schneider <marco.schneider@active-group.de>
2025-12-09 14:46:01 +0100

    marvin: Set xdg variables.

--- hosts/marvin/home.nix
+++ hosts/marvin/home.nix
@@ -3,4 +3,11 @@
   imports = [
     ../common.nix
   ];
+
+  home.sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
 }
