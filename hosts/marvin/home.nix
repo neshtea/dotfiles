945b382045dbd733c08698fac49749b5ceaf2b88
Marco Schneider <marco.schneider@active-group.de>
2025-12-17 10:08:13 +0100

    marvin: Enable bash.

--- hosts/marvin/home.nix
+++ hosts/marvin/home.nix
@@ -3,13 +3,5 @@
   imports = [
     ../common.nix
   ];
-
-  home.sessionVariables = {
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_STATE_HOME = "$HOME/.local/state";
-  };
-
-  modules.programs.neovim.enable = true;
+  programs.bash.enable = true;
 }
