a82b2040d969a9033f9bfe8200460cf8630ae93e
Marco Schneider <marco.schneider@active-group.de>
2026-03-10 09:59:18 +0100

    neovim: Erlang formatter.

--- modules/programs/neovim/nvim/after/plugin/conform.lua
+++ modules/programs/neovim/nvim/after/plugin/conform.lua
@@ -10,6 +10,7 @@ conform.setup({
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        erlang = { "erlfmt", stop_after_first = true },
     },
     format_on_save = { timeout_ms = 500 },
 })
