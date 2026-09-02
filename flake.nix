a55cdc19bb83541e84d6493f4819671d82ba31cc
Marco Schneider <marco.schneider@active-group.de>
2026-09-02 09:41:11 +0200

    flake: Pull out `mkHome` helper function.

--- flake.nix
+++ flake.nix
@@ -23,82 +23,83 @@
   };
 
   outputs =
-    inputs@{ nixpkgs, home-manager, sops-nix, ... }:
+    inputs@{
+      nixpkgs,
+      home-manager,
+      sops-nix,
+      ...
+    }:
     let
       username = "schneider"; # conveniently, this is my username on all systems.
       # Put things in `specialArgs` that we might need for
       # configuration further down the configuration (for example in
       # hm modules).
       specialArgs = { inherit inputs; };
+
+      mkHome =
+        {
+          system,
+          user ? username,
+          host,
+          homeDirectory,
+        }:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          extraSpecialArgs = specialArgs;
+          modules = [
+            ./hosts/${host}/home.nix
+            {
+              home = {
+                username = user;
+                inherit homeDirectory;
+                stateVersion = "22.05";
+              };
+            }
+          ];
+        };
     in
     {
       homeConfigurations = {
-        "${username}@wayfarer" =
-          let
-            pkgs = import nixpkgs {
-              config.allowUnfree = true; # Sorry rms
-              system = "aarch64-darwin";
-            };
-          in
-          home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = specialArgs;
-            modules = [
-              ./hosts/wayfarer/home.nix
-              {
-                home = {
-                  inherit username;
-                  homeDirectory = "/Users/${username}";
-                  stateVersion = "22.05";
-                };
-              }
-            ];
-          };
-        "pi@marvin" =
-          let
-            pkgs = import nixpkgs {
-              config.allowUnfree = true;
-              system = "aarch64-linux";
-            };
-          in
-          home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs = specialArgs;
-            modules = [
-              ./hosts/marvin/home.nix
-              {
-                home = {
-                  username = "pi";
-                  homeDirectory = "/home/pi";
-                  stateVersion = "22.05";
-                };
-              }
-            ];
-          };
+        "${username}@wayfarer" = mkHome {
+          system = "aarch64-darwin";
+          host = "wayfarer";
+          homeDirectory = "/Users/${username}";
+        };
+
+        "pi@marvin" = mkHome {
+          system = "aarch64-linux";
+          host = "marvin";
+          homeDirectory = "/home/${username}";
+        };
       };
       nixosConfigurations = {
         hetzner-lab =
-          let system = "x86_64-linux";
-              pkgs = import nixpkgs {
-                inherit system;
-              };
-          in
-            nixpkgs.lib.nixosSystem {
-              inherit pkgs system specialArgs;
-              modules = [
-                ./hosts/hetzner-lab/configuration.nix
-                sops-nix.nixosModules.sops
-                home-manager.nixosModules.home-manager
-                {
-                  home-manager = {
-                    users."marco" = import ./hosts/hetzner-lab/home.nix;
-                    useGlobalPkgs = true;
-                    useUserPackages = false;
-                    extraSpecialArgs = specialArgs;
-                  };
-                }
-              ];
+          let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
             };
+          in
+          nixpkgs.lib.nixosSystem {
+            inherit pkgs system specialArgs;
+            modules = [
+              ./hosts/hetzner-lab/configuration.nix
+              sops-nix.nixosModules.sops
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  users."marco" = import ./hosts/hetzner-lab/home.nix;
+                  useGlobalPkgs = true;
+                  useUserPackages = false;
+                  extraSpecialArgs = specialArgs;
+                };
+              }
+            ];
+          };
         oxomoco =
           let
             system = "x86_64-linux";
@@ -107,22 +108,22 @@
               inherit system;
             };
           in
-            nixpkgs.lib.nixosSystem {
-              inherit pkgs system specialArgs;
-              modules = [
-                ./hosts/oxomoco/configuration.nix
-                home-manager.nixosModules.home-manager
-                {
-                  home-manager = {
-                    users.${username} = import ./hosts/oxomoco/home.nix;
-                    useGlobalPkgs = true;
-                    useUserPackages = false;
-                    extraSpecialArgs = specialArgs;
-                  };
-                }
-                inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
-              ];
-            };
+          nixpkgs.lib.nixosSystem {
+            inherit pkgs system specialArgs;
+            modules = [
+              ./hosts/oxomoco/configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  users.${username} = import ./hosts/oxomoco/home.nix;
+                  useGlobalPkgs = true;
+                  useUserPackages = false;
+                  extraSpecialArgs = specialArgs;
+                };
+              }
+              inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
+            ];
+          };
       };
     };
 }
