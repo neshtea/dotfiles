31b8d43b192b079d5f637f1554245621b24c2aff
Marco Schneider <marco.schneider@active-group.de>
2026-09-02 08:27:23 +0200

    oxomoco: Cleanup session vars.

--- hosts/oxomoco/configuration.nix
+++ hosts/oxomoco/configuration.nix
@@ -117,22 +117,14 @@
       kdePackages.kpat
       kdePackages.ksudoku
     ];
-    sessionVariables = rec {
-      XDG_CACHE_HOME = "$HOME/.cache";
-      XDG_CONFIG_HOME = "$HOME/.config";
-      XDG_DATA_HOME = "$HOME/.local/share";
-      XDG_STATE_HOME = "$HOME/.local/state";
-
-      # Not officially in the specification
-      XDG_BIN_HOME = "$HOME/.local/bin";
-      PATH = [ "${XDG_BIN_HOME}" ];
-      # Optional, hint electron apps to use wayland:
-      NIXOS_OZONE_WL = "1";
-    };
+    # Optional, hint electron apps to use wayland:
+    sessionVariables.NIXOS_OZONE_WL = "1";
   };
 
-  virtualisation.docker.enable = true;
-  virtualisation.podman.enable = true;
+  virtualisation = {
+    docker.enable = true;
+    podman.enable = true;
+  };
 
   programs = {
     gnupg.agent = {
